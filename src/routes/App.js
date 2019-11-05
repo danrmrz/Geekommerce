54c8929f8905873616d9c8cddd83fd6834b97f87
Jose Oscar <orosasp@gmail.com>
2019-11-04 23:56:29 -0600

    Solucione el conflicto de las ramas al fusionar

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
-<<<<<<< HEAD
-=======
 import PoliticasPrivacidad from '../containers/PoliticasPrivacidad';
 import TerminosCondiciones from '../containers/TerminosCondiciones';
 import Acercade from '../containers/Acercade';
->>>>>>> footer
 import NotFoud from '../containers/NotFound';
 import Layout from '../components/Layout';
 
@@ -14,14 +11,10 @@ const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-<<<<<<< HEAD
-        <Route exact path='/' component={Home} />
-=======
         <Route exact path="/" component={Home} />
         <Route exact path="/politicasPrivacidad" component={PoliticasPrivacidad} />
         <Route exact path="/terminosycondiciones" component={TerminosCondiciones} />
         <Route exact path="/acercade" component={Acercade} />
->>>>>>> footer
         <Route component={NotFoud} />
       </Switch>
     </Layout>
